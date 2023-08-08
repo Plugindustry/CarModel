ad9f6e348cca3ab9a1dc9748c3b98538f253f7ff
czm23333 <34388355+czm23333@users.noreply.github.com>
2023-08-08 19:10:09 +0800

    Corresponding change

--- src/main/java/io/github/plugindustry/carmodel/item/DarkTalisman.java
+++ src/main/java/io/github/plugindustry/carmodel/item/DarkTalisman.java
@@ -1,9 +1,11 @@
 package io.github.plugindustry.carmodel.item;
 
-import io.github.plugindustry.carmodel.ConstItem;
 import io.github.plugindustry.wheelcore.interfaces.item.DummyItem;
 import io.github.plugindustry.wheelcore.manager.EntityDamageHandler;
+import io.github.plugindustry.wheelcore.utils.InventoryUtil;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageEvent;
 
@@ -17,12 +19,14 @@ public class DarkTalisman extends DummyItem {
         EntityDamageHandler.infoModifiers.add(new EntityDamageHandler.DamageInfoModifier() {
             @Override
             public boolean modify(@Nonnull EntityDamageEvent.DamageCause cause,
-                    @Nonnull EntityDamageHandler.DamageInfo info, boolean canBlock, @Nullable Entity damager,
-                    @Nonnull Entity damagee) {
-                if (damagee instanceof Player player && player.getInventory().contains(ConstItem.DARK_TALISMAN) &&
+                    @Nonnull EntityDamageHandler.DamageInfo info, boolean canBlock, @Nullable Entity damagerEntity,
+                    @Nullable Block damagerBlock, @Nonnull Entity damagee) {
+                if (damagee instanceof Player player &&
+                    InventoryUtil.contains(player.getInventory(), DarkTalisman.INSTANCE) &&
                     (cause == EntityDamageEvent.DamageCause.MAGIC || cause == EntityDamageEvent.DamageCause.POISON ||
-                     cause == EntityDamageEvent.DamageCause.WITHER || cause == EntityDamageEvent.DamageCause.THORNS))
-                    info.applyArmor = true;
+                     cause == EntityDamageEvent.DamageCause.WITHER || cause == EntityDamageEvent.DamageCause.THORNS ||
+                     (cause == EntityDamageEvent.DamageCause.ENTITY_ATTACK && damagerEntity != null &&
+                      damagerEntity.getType() == EntityType.AREA_EFFECT_CLOUD))) info.applyArmor = true;
                 info.damage *= 1.2;
                 return true;
             }
